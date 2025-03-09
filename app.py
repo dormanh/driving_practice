676adbc964461cee5842a977fbb9b31a569e3723
Hanga Dormán <dormanhanga@gmail.com>
2025-03-09 14:42:36 +0100

    make sequence of lights realistic

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import dash_bootstrap_components as dbc
 
-from dash import Dash, Input, Output, dcc, html
+from dash import Dash, Input, Output, State, dcc, html
 
 app = Dash(
     __name__,
@@ -16,15 +16,23 @@ app = Dash(
     suppress_callback_exceptions=True,
 )
 
-light_path = lambda spec: f"assets/{spec}.png"
 n = 3
+
+multiple_freq = 0.5
 multiple_interval = 2000
-single_interval = 5000
-freq = 0.5
+min_dead_time_multiple = 2000
+max_dead_time_multiple = 5000
 time_since_last_flash_multiple = 0
+sequence = ["red_yellow", "green", "yellow", "red"]
+current_state = 0
+
+single_freq = 0.1
+single_interval = 5000
+min_dead_time_single = 4000
+max_dead_time_single = 20000
 time_since_last_flash_single = 0
-min_dead_time = 2000
-max_dead_time = 5000
+
+light_path = lambda spec: f"assets/{spec}.png"
 
 
 app.layout = html.Div(
@@ -75,45 +83,41 @@ app.layout = html.Div(
 
 @app.callback(
     *[Output(f"light_{i}", "src") for i in range(n)],
+    *[State(f"light_{i}", "src") for i in range(n)],
     Input("multiple_interval", "n_intervals"),
 )
-def update_lights(n_intervals: int) -> list:
-    global time_since_last_flash_multiple
+def update_lights(light1: str, light2: str, light3: str, _: int) -> list:
+    global time_since_last_flash_multiple, current_state
     time_since_last_flash_multiple += multiple_interval
-    if (
-        (n_intervals is not None)
-        & (min_dead_time < time_since_last_flash_multiple)
-        & (
-            np.random.binomial(1, freq)
-            | (max_dead_time < time_since_last_flash_multiple)
-        )
-    ):
+    do_change = np.random.binomial(1, multiple_freq)
+    past_min_dead_time = min_dead_time_multiple < time_since_last_flash_multiple
+    past_max_dead_time = max_dead_time_multiple < time_since_last_flash_multiple
+
+    if past_min_dead_time and (do_change or past_max_dead_time):
         time_since_last_flash_multiple = 0
-        light_on = np.random.choice(np.arange(3))
-        color = np.random.choice(["red", "yellow", "green"])
+        which_lamp_on = np.random.choice(range(n))
+        color = sequence[current_state % 4]
+        current_state += 1
         return [
-            light_path(color) if (i == light_on) else light_path("off")
+            light_path(color) if i == which_lamp_on else light_path("off")
             for i in range(n)
         ]
     else:
-        return [light_path("off") for _ in range(n)]
+        return [light_path(light.split("/")[1].removesuffix(".png")) for light in [light1, light2, light3]]
 
 
 @app.callback(
     Output("single_light", "src"),
     Input("single_interval", "n_intervals"),
 )
-def update_light(n_intervals: int) -> list:
+def update_light(_: int) -> list:
     global time_since_last_flash_single
     time_since_last_flash_single += single_interval
-    if (
-        (n_intervals is not None)
-        & (min_dead_time * 2 < time_since_last_flash_single)
-        & (
-            np.random.binomial(1, freq / 5)
-            | (max_dead_time * 4 < time_since_last_flash_single)
-        )
-    ):
+    do_change = np.random.binomial(1, single_freq)
+    past_min_dead_time = min_dead_time_single < time_since_last_flash_single
+    past_max_dead_time = max_dead_time_single < time_since_last_flash_single
+
+    if past_min_dead_time and (do_change or past_max_dead_time):
         time_since_last_flash_single = 0
         return light_path("single_on")
     else:
